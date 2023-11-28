8e1da4e1d190fc5bf35525b7448c8faa444465a0
Mochamad Ludfi Rahman <ludfyrahman@192.168.1.6>
2023-11-28 22:00:02 +0700

    update landing page until consultation

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -127,59 +127,55 @@
 
 <section class="Feautes section mt-2">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="section-title">
-                    <h2>Ini yang NCC sediakan untukmu.</h2>
-                    <img src="img/section-img.png" alt="#">
-                    <p>Nutrition Care Center (NCC) memiliki berbagai layanan untuk memberikan panduan dan nasihat terbaik dalam perawatan gizi Anda. </p>
-                </div>
+        <div class="row px-2 py-3">
+            <div class="col-lg-6">
+                <h1>Ini yang NCC sediakan untukmu.</h1>
+                {{-- <img src="{{asset('fe_assets/img/section-img.png')}}" alt="#"> --}}
+            </div>
+            <div class="col-md-6">
+                <p>Nutrition Care Center (NCC) memiliki berbagai layanan untuk memberikan panduan dan nasihat terbaik dalam perawatan gizi Anda. </p>
             </div>
         </div>
+    </div>
+</section>
+<section class="our-vision-area ptb-100 pt-0">
+    <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-12">
-
-                <div class="single-features">
-                    <div class="signle-icon">
-                        <i class="icofont icofont-ambulance-cross"></i>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-vision-box">
+                    <div class="icon">
+                        <img src="{{asset('fe_assets/img/organik.png')}}" alt="#">
                     </div>
                     <h3>Resep Organik</h3>
                     <p>Temukan resep organik yang lezat dan sehat untuk mendukung gaya hidup Anda yang ramah lingkungan.</p>
                 </div>
-
             </div>
-            <div class="col-lg-3 col-12">
-
-                <div class="single-features">
-                    <div class="signle-icon">
-                        <i class="icofont icofont-medical-sign-alt"></i>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-vision-box">
+                    <div class="icon">
+                        <img src="{{asset('fe_assets/img/nutrisi.png')}}" alt="#">
                     </div>
                     <h3>Strategi Nutrisi</h3>
                     <p>Dapatkan strategi nutrisi yang tepat untuk mencapai tujuan kesehatan dan kebugaran Anda.</p>
                 </div>
-
             </div>
-            <div class="col-lg-3 col-12">
-
-                <div class="single-features last">
-                    <div class="signle-icon">
-                        <i class="icofont icofont-stethoscope"></i>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-vision-box">
+                    <div class="icon">
+                        <img src="{{asset('fe_assets/img/kesehatan.png')}}" alt="#">
                     </div>
                     <h3>Tingkatkan Kesehatan</h3>
                     <p>Tingkatkan kesehatan Anda dengan panduan gizi yang disesuaikan untuk meningkatkan energi.</p>
                 </div>
-
             </div>
-            <div class="col-lg-3 col-12">
-
-                <div class="single-features last">
-                    <div class="signle-icon">
-                        <i class="icofont icofont-stethoscope"></i>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="single-vision-box">
+                    <div class="icon">
+                        <img src="{{asset('fe_assets/img/bmi.png')}}" alt="#">
                     </div>
                     <h3>Kalkulator BMI</h3>
                     <p>Gunakan kalkulator BMI kami untuk memantau dan mengelola berat badan secara efektif.</p>
                 </div>
-
             </div>
         </div>
     </div>
@@ -241,38 +237,6 @@
 <section class="why-choose section">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="section-title">
-                    <h2>Konsultasikan Kesehatanmu dengan Ahli Gizi Terpercaya Melalui Layanan Online di NCC!</h2>
-                    <img src="{{asset('fe_assets/img/section-img.png')}}" alt="#">
-                    <p>Di Nutrition Care Center (NCC), kami mengembangkan program kesehatan yang memberikan peluang bagi Anda untuk menjadi lebih efisien dan fokus dalam mencapai tujuan kesehatan Anda. </p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 col-12">
-
-                <div class="choose-left">
-                    <h3>Who We Are</h3>
-                    <p>Di Nutrition Care Center (NCC), kami mengembangkan program kesehatan yang memberikan peluang bagi Anda untuk menjadi lebih efisien dan fokus dalam mencapai tujuan kesehatan Anda.</p>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list">
-                                <li><i class="fa fa-caret-right"></i>Ahli Gizi Terpercaya</li>
-                                <li><i class="fa fa-caret-right"></i>Keahlian Profesional</li>
-                                <li><i class="fa fa-caret-right"></i>Pendekatan Personalized</li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list">
-                                <li><i class="fa fa-caret-right"></i>Pengetahuan Mendalam</li>
-                                <li><i class="fa fa-caret-right"></i>Bimbingan Motivasi</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
             <div class="col-lg-6 col-12">
 
                 <div class="choose-right">
@@ -288,6 +252,29 @@
 
                         <a href="https://www.youtube.com/watch?v=RFVXy6CRVR4"
                             class="video video-popup mfp-iframe"><i class="fa fa-play"></i></a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-6 col-12">
+
+                <div class="choose-left">
+                    <h3>Konsultasikan Kesehatanmu Dengan Ahli Gizi Terpercaya Melalui Layanan Online Di NCC!</h3>
+                    <p>Di Nutrition Care Center (NCC), kami mengembangkan program kesehatan yang memberikan peluang bagi Anda untuk menjadi lebih efisien dan fokus dalam mencapai tujuan kesehatan Anda. </p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list">
+                                <li><i class="fa fa-caret-right"></i>Ahli Gizi Terpercaya</li>
+                                <li><i class="fa fa-caret-right"></i>Keahlian Profesional</li>
+                                <li><i class="fa fa-caret-right"></i>Pendekatan Personalized</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul class="list">
+                                <li><i class="fa fa-caret-right"></i>Pengetahuan Mendalam</li>
+                                <li><i class="fa fa-caret-right"></i>Bimbingan Motivasi</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
